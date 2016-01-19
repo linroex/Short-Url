6140aeb1639cf35542488fe5f60fe0195f9f26e6
linroex <linroex@coder.tw>
2016-01-19 22:45:04 +0800

    send verify mail for ntust.me email service

--- Config.sample.py
+++ Config.sample.py
@@ -1,5 +1,10 @@
 config = {
-    'db_connection_string': 'mysql+mysqlconnector://',
-    'add_quota_per': 100,
-    'offset': 0
+    'db_connection_string': 'mysql+mysqlconnector://shortenurl:Taj0moV2Ab0Wyb6I@localhost/shorturl',
+    'add_quota_per': 300,
+    'offset': 0,
+    'API_URL': 'https://api.mailgun.net/v3',
+    'API_KEY': '',
+    'DOMAIN': '',
+    'SENDER': '',
+    'secret': ''
 }
--- ntust_me_mail.py
+++ ntust_me_mail.py
@@ -2,15 +2,19 @@ import csv
 import requests
 
 from requests.auth import HTTPBasicAuth
+from Config import config
 
-API_URL = 'https://api.mailgun.net/v3'
-API_KEY = 'key-80tmpp2v51qj6jd234d4g7hhqkrmtrf1'
-DOMAIN = 'ntust.me'
-SENDER = 'no-reply@ntust.me'
+def generate_verify_token(email):
+    import hashlib
+    from time import time
+
+    return hashlib.sha224((email + str(time()) + config['secret']).encode('utf8')).hexdigest()
 
 def get_random_password(length):
     from random import choice
     from string import ascii_uppercase
+
+    
 
     return ''.join([choice(ascii_uppercase) for c in range(length)])
 
@@ -21,29 +25,30 @@ def appliers_handler(appliers_file):
         appliers = csv.reader(f)
 
         for applier in appliers:
-            # login = applier[2]
+            name = applier[1]
             email = applier[3]
-            # password = get_random_password(8)
+            token = generate_verify_token(email)
             
-            send_verify_mail(email)
+            send_verify_mail(email, name, token)
+            exit()
 
 def add_smtp_credentials(login, password):
     return requests.post(
-        '{API_URL}/domains/{DOMAIN}/credentials'.format(API_URL=API_URL, DOMAIN=DOMAIN), 
-        auth = HTTPBasicAuth('api', API_KEY), 
+        '{API_URL}/domains/{DOMAIN}/credentials'.format(API_URL=config['API_URL'], DOMAIN=config['DOMAIN']), 
+        auth = HTTPBasicAuth('api', config['API_KEY']), 
         data = {
         'login': login,
         'password': password
     })
 
-def send_verify_mail(receiver):
+def send_verify_mail(receiver, name, token):
     subject = 'NTUST.ME 電子信箱申請確認信'
-    content = open('verify_mail.html', encoding='utf-8').read()
+    content = open('verify_mail.html', encoding='utf-8').read().replace('{{name}}', name).replace('{{token}}', token)
 
     return requests.post(
-        '{API_URL}/domains/{DOMAIN}/credentials'.format(API_URL=API_URL, DOMAIN=DOMAIN),
-        auth=("api", API_KEY),
-        data={"from": SENDER,
+        '{API_URL}/{DOMAIN}/messages'.format(API_URL=config['API_URL'], DOMAIN=config['DOMAIN']),
+        auth=("api", config['API_KEY']),
+        data={"from": config['SENDER'],
             "to": receiver,
             "subject": subject,
             "html": content,
